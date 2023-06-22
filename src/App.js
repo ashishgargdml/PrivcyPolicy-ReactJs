758ce9fa26fb3e6e4545d94bbdccb47eb4f390ac
Ashish Garg <ashish.garg@digimantra.com>
2023-06-22 11:50:39 +0530

    css update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import PrivacyPolicy from './component/Privacy-Policy';
-import Terms from './component/Terms';
-import Support from './component/Support';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import PrivacyPolicy from "./component/Privacy-Policy";
+import Terms from "./component/Terms";
+import Support from "./component/Support";
+import "./App.css";
 
 const App = () => {
   return (
     <>
-    <div>
-      <li><Link to='/privacy-policy'>Privacy Policy</Link></li>
-      <li><Link to='/terms'>Terms</Link></li>
-      <li><Link to='/support'>Support</Link></li>
-    </div>
-    <Routes>
-      <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-      <Route path='/terms' element={<Terms />} />
-      <Route path='/support' element={<Support />} />
+      <div>
+        <li>
+          <Link to="/privacy-policy">Privacy Policy</Link>
+        </li>
+        <li>
+          <Link to="/terms">Terms</Link>
+        </li>
+        <li>
+          <Link to="/support">Support</Link>
+        </li>
+      </div>
+      <Routes>
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/Support.js
+++ src/component/Support.js
@@ -2,10 +2,15 @@ import React from "react";
 
 const Support = () => {
   return (
-    <div>
-      "Playdates was created to bring people together in real life for whatever
-      reason you might have. Whether that's for something impromptu or planned.
-      Enjoy. Email: support@playdatesapp.us "
+    <div className="support-container">
+      <h1 className="policy-heading">Support</h1>
+      <div className="policy-text">
+        "Playdates was created to bring people together in real life for
+        whatever reason you might have. Whether that's for something impromptu
+        or planned."
+      </div>
+      Email:{" "}
+      <a href="mailto: support@playdatesapp.us">support@playdatesapp.us</a>
     </div>
   );
 };
